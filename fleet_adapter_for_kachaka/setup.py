3ed09978f3323a713dca2de78d323e077a288c7c
h-wata <hirofumi01.watanabe@g.softbank.co.jp>
2024-02-15 17:07:22 +0900

    Add delivery node

--- fleet_adapter_for_kachaka/setup.py
+++ fleet_adapter_for_kachaka/setup.py
@@ -1,3 +1,6 @@
+import os
+from glob import glob
+
 from setuptools import setup
 
 package_name = 'fleet_adapter_for_kachaka'
@@ -10,7 +13,11 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        ('share/' + package_name,['config.yaml']),
+        ('share/' + package_name, ['config.yaml']),
+        (
+            os.path.join('share', package_name, 'launch'),
+            glob('launch/*.launch.xml'),
+        ),
 
     ],
     install_requires=['setuptools'],
@@ -22,7 +29,8 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'fleet_adapter=fleet_adapter_for_kachaka.fleet_adapter:main'
+            'fleet_adapter=fleet_adapter_for_kachaka.fleet_adapter:main',
+            'delivery_node=fleet_adapter_for_kachaka.delivery_node:main',
         ],
     },
 )
