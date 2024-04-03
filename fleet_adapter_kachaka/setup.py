1673ceddaf3a8d53a4d1dc8ebbdbb1de95f450a2
h-wata <hirofumi01.watanabe@g.softbank.co.jp>
2024-04-03 15:28:46 +0900

    fix maintainer

--- fleet_adapter_kachaka/setup.py
+++ fleet_adapter_kachaka/setup.py
@@ -1,4 +1,22 @@
-import os  # noqa: C101
+#!/usr/bin/env python3
+# -*- encoding: utf-8 -*-
+
+# Copyright (c) 2024 SoftBank Corp.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+import os
 from glob import glob
 
 from setuptools import setup
@@ -22,8 +40,8 @@ setup(
     ],
     install_requires=['setuptools'],
     zip_safe=True,
-    maintainer='Yadunund',
-    maintainer_email='yadunund@openrobotics.org',
+    maintainer='SoftBank Corp.',
+    maintainer_email='SBGRP-git@g.softbank.co.jp',
     description='A RMF fleet adapter for kachaka',
     license='Apache License 2.0',
     tests_require=['pytest'],
